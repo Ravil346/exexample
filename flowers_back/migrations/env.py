722d4f6e5ddd5cd57c7300a0a35c8c20b001946f
s.zakharov <s.zakharov@mixvel.com>
2025-05-02 03:39:02 +0300

    Initial commit for public repository

--- flowers_back/migrations/env.py
+++ flowers_back/migrations/env.py
@@ -1,18 +1,9 @@
-import sys
-import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
-# Добавляем корневую директорию проекта в sys.path
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# Импортируем модели и базу данных
-from app.core.database import Base
-from app.models import product, shop, order, category, user, consultation, pay
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -27,7 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -67,7 +58,7 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
